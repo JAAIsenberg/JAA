4241da986038a3468b081dd769b449948cf863fd
Alejandro <65742076+amolinae06@users.noreply.github.com>
2022-10-19 08:42:32 -0500

    asdidsad

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+void browser(){
+	
+}
+
+void firefoxmoment(){
+	//while(true){
+		system("firefox --new-window https://www.reddit.com");
+			/*if (true == true){
+				break;
+			}
+	}*/
+}
+
+void chromiummoment(){
+
+}
+
+int main(){
+	firefoxmoment();
+}
