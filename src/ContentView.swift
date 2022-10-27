03b2f4f9f45577f8e660eb515e498840561aa33c
A-drianr06 <116178786+A-drianr06@users.noreply.github.com>
2022-10-27 10:22:23 -0500

    Update ContentView.swift

--- src/ContentView.swift
+++ src/ContentView.swift
@@ -7,12 +7,7 @@ struct ContentView: View {
                 Spacer()
                 MenudeSeleccion()
                 Spacer()
-                RecienAgregadas()
-                Recientes()
-                Populares()
-                Favoritas()
-                Spacer()
-                MiniPlayer()
+                Inicio()
                 Spacer()
             }
         }
@@ -20,35 +15,23 @@ struct ContentView: View {
         .background(.black)
     }
     
-    struct TopView: View {
-        var body: some View {
-            HStack(alignment: .center, spacing: 90){
-                Text("JAA")
-                    .foregroundColor(.white)
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 96, height: 96, alignment: .topLeading)
-            }
-        }
-    }
-    
     struct MenudeSeleccion: View {
         @State private var ShowSettings = false
+        @State private var ShowAlbums = false
         var body: some View {
             VStack(alignment: .center){
                 HStack(spacing: 10){
                     Button("Inicio"){}
                         .buttonStyle(.bordered)
                         .foregroundColor(.accentColor)
-                        
-                        Button("Albums"){}
-                            .buttonStyle(.bordered)
-                            .foregroundColor(.accentColor)
-                        
-                        Button("Playlists"){}
-                            .buttonStyle(.bordered)
-                            .foregroundColor(.accentColor)
+                    
+                    Button("Albums"){}
+                        .buttonStyle(.bordered)
+                        .foregroundColor(.accentColor)
+                    
+                    Button("Playlists"){}
+                        .buttonStyle(.bordered)
+                        .foregroundColor(.accentColor)
                     
                     Button(action: {ShowSettings.toggle()}){
                         Image(systemName: "gear")
@@ -59,133 +42,12 @@ struct ContentView: View {
                     if ShowSettings {
                         Settings()
                     }
+                    if ShowAlbums{
+                        Albums()
+                    }
                     
-                    }.padding(.leading, 30)
-                }
+                }.padding(.leading, 30)
             }
         }
-        
-        struct Recientes: View {
-            var body: some View {
-                VStack(alignment: .leading){
-                    Text("Escuchadas Recientemente")
-                    HStack{
-                        Button(action: {}){
-                            Image("album01")
-                                .resizable()
-                                .frame(width: 128, height: 128, alignment: .topLeading)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                        }
-                        Button(action: {}){
-                            Image("album02")
-                                .resizable()
-                                .frame(width: 128, height: 128, alignment: .topLeading)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                        }
-                        Button(action: {}){
-                            Image("album03")
-                                .resizable()
-                                .frame(width: 128, height: 128, alignment: .topLeading)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                        }
-                    }
-                }
-            }
-        }
-        
-        struct Populares: View {
-            var body: some View {
-                VStack(alignment: .leading){
-                    Text("Populares")
-                    HStack{
-                        Button(action: {}){
-                            Image("album06")
-                                .resizable()
-                                .frame(width: 128, height: 128, alignment: .topLeading)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                        }
-                        Button(action: {}){
-                            Image("album07")
-                                .resizable()
-                                .frame(width: 128, height: 128, alignment: .topLeading)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                        }
-                        Button(action: {}){
-                            Image("album08")
-                                .resizable()
-                                .frame(width: 128, height: 128, alignment: .topLeading)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                        }
-                    }
-                }
-            }
-        }
-        
-        struct Favoritas: View {
-            var body: some View {
-                VStack(alignment: .leading){
-                    Text("Favoritas")
-                    HStack{
-                        Button(action: {}){
-                            Image("album05")
-                                .resizable()
-                                .frame(width: 128, height: 128, alignment: .topLeading)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                        }
-                        Button(action: {}){
-                            Image("album09")
-                                .resizable()
-                                .frame(width: 128, height: 128, alignment: .topLeading)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                        }
-                        Button(action: {}){
-                            Image("album11")
-                                .resizable()
-                                .frame(width: 128, height: 128, alignment: .topLeading)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                        }
-                    }
-                }
-            }
-        }
-        
-        struct RecienAgregadas: View {
-            var body: some View {
-                VStack(alignment: .leading){
-                    Text("Agregadas Recientemente")
-                    HStack{
-                        Button(action: {}){
-                            Image("album12")
-                                .resizable()
-                                .frame(width: 128, height: 128, alignment: .topLeading)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                        } 
-                        Button(action: {}){
-                            Image("album10")
-                                .resizable()
-                                .frame(width: 128, height: 128, alignment: .topLeading)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                        }
-                        Button(action: {}){
-                            Image("album04")
-                                .resizable()
-                                .frame(width: 128, height: 128, alignment: .topLeading)
-                                .scaledToFit()
-                                .clipShape(RoundedRectangle(cornerRadius: 16))
-                        }
-                    }
-                }
-            } 
-        }   
+    }
 }
