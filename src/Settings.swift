42dcd9cb05747ced38f2bc7024ac9dca7e84ebbe
A-drianr06 <116178786+A-drianr06@users.noreply.github.com>
2022-10-27 10:24:37 -0500

    Update 2

--- src/Settings.swift
+++ src/Settings.swift
@@ -1,8 +1,26 @@
 import SwiftUI
 
-public struct Settings: View{
-    public init(){}
-    public var body: some View {
-        Text("Opciones")
+struct Settings: View{
+    @State var isOn: Bool = false
+    var body: some View {
+        NavigationView{
+        HStack{
+            VStack{
+                ScrollView{
+                Text("Opciones")
+                    .buttonStyle(.bordered)
+                    .foregroundColor(.accentColor)
+                Button(action: {}){
+                    Text("Salir")
+                }
+                .buttonStyle(.bordered)
+                .foregroundColor(.accentColor)
+                
+                Toggle("Modo Online", isOn: $isOn)
+                }
+            }
+        }
+        .background(.gray.opacity(0.5))
+        }
     }
 }
