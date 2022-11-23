642525d01d8124b02b324defa49160b0149bd4d5
Alejandro <65742076+amolinae06@users.noreply.github.com>
2022-11-22 22:21:46 -0600

    Update and rename albums.swift to Albums.swift

---
+++ src/Albums.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+struct Albums: View {
+    var body: some View{
+        NavigationView(){
+            Text("Hi")
+        }
+        .navigationBarTitle("Albums", displayMode: .inline)
+    }
+} 
